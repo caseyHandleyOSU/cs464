5d21661194c43663779b0c176fdc9e72a7fd818e
caseyHandleyOSU <handlcas@oregonstate.edu>
2026-04-03 14:22:46 -0700

    File read efficiency upgrades Uses dirent from `fs.readdir` to avoid unnecessary sync call Uses `Promise.all` to read files concurrently

--- src/app/api/data/route.ts
+++ src/app/api/data/route.ts
@@ -5,19 +5,19 @@ import { DataEntry } from "@/types/data";
 const DATA_DIRECTORY = path.join(process.cwd(), "public", "data");
 
 export async function GET(request: Request) {
-  const files = await fs.readdir(DATA_DIRECTORY)
-  const data: DataEntry[] = []
-  for(const file of files) {
-    const filePath = path.join(DATA_DIRECTORY, file)
+  const items = await fs.readdir(DATA_DIRECTORY, { withFileTypes: true })
+  const promises = await Promise.all(items.map(item => {
     // Skip directories
-    const stat = await fs.stat(filePath)
-    if(!stat.isFile()) continue
+    if (!item.isFile()) return
     // Skip non-JSON files
-    if(path.extname(file) !== ".json") continue
+    if (path.extname(item.name) !== ".json") return
     
-    const fileData = await fs.readFile(filePath, "utf-8")
-    data.push(JSON.parse(fileData) as DataEntry)
-  }
+    const filePath = path.join(DATA_DIRECTORY, item.name)
+    return fs.readFile(filePath, "utf-8")
+  }))
+  const data: DataEntry[] = promises
+    .filter(f => typeof f === 'string')
+    .map((fileData) => JSON.parse(fileData)) as DataEntry[]
 
   return Response.json(data)
 }
