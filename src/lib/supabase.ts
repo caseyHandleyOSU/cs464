8fd33cc99e317c4706e933edfb16c535e87cb3e3
caseyHandleyOSU <handlcas@oregonstate.edu>
2026-05-14 15:25:10 -0700

    feat: use authenticated connection for `getSupabaseClient()`

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -1,20 +1,25 @@
-import { Database } from "@/types/supabase"
+import { createServerClient } from "./supabase/createServerClient"
 import { createClient } from "@supabase/supabase-js"
+import { Database } from "@/types/supabase"
 
 let supabaseClient: ReturnType<typeof createClient<Database>> | null = null
 
 export function getSupabaseClient() {
-    if (supabaseClient) {
-        return supabaseClient
-    }
+  return createServerClient()
+}
 
-    const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL
-    const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_PUBLISHABLE_KEY
-
-    if (!supabaseUrl || !supabaseKey) {
-        throw new Error('Missing Supabase environment variables')
-    }
-
-    supabaseClient = createClient<Database>(supabaseUrl, supabaseKey)
+export function getSupabaseClientNoAuth() {
+  if (supabaseClient) {
     return supabaseClient
+  }
+
+  const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL
+  const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_PUBLISHABLE_KEY
+
+  if (!supabaseUrl || !supabaseKey) {
+    throw new Error('Missing Supabase environment variables')
+  }
+
+  supabaseClient = createClient<Database>(supabaseUrl, supabaseKey)
+  return supabaseClient
 }
