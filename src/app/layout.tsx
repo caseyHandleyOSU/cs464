5ab88128793ce27701aad0370f2aa2b6db96f66c
caseyHandleyOSU <handlcas@oregonstate.edu>
2026-05-14 15:15:49 -0700

    add logout button to layout not completed; only for testing

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,18 +1,26 @@
-import type { Metadata } from "next";
+import { createServerClient } from "@/lib/supabase/createServerClient"
+import type { Metadata } from "next"
+import { LogoutButton } from "@/components/supabase/logout-button"
 
 export const metadata: Metadata = {
   title: "CS464 Project",
   description: "Yet-to-be-named project for cs464",
 };
 
-export default function RootLayout({
+export default async function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+  const client = await createServerClient()
+  const { data } = await client.auth.getUser()
+  const { user } = data
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body>
+        { user ? <LogoutButton /> : <></> }
+        {children}
+      </body>
     </html>
   );
 }
